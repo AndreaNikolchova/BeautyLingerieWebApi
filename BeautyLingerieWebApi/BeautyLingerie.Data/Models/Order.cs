564b3f5402e7545fb63717f6c33eda49e075ab81
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-01 16:04:50 +0300

    fixed order model for guest and registered order

--- BeautyLingerieWebApi/BeautyLingerie.Data/Models/Order.cs
+++ BeautyLingerieWebApi/BeautyLingerie.Data/Models/Order.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BeautyLingerie.Data.Models
 {
@@ -7,12 +10,35 @@ namespace BeautyLingerie.Data.Models
         public Order()
         {
             this.OrderId = Guid.NewGuid();
-            this.Products =  new List<Product>();   
+            this.Products = new List<Product>();
         }
+
         public Guid OrderId { get; set; }
+
+        [Required]
         public decimal TotalSum { get; set; }
+
+        [Required]
         public DateTime CreatedOn { get; set; }
+
         public IReadOnlyCollection<Product> Products { get; set; }
 
+        public Guid? UserId { get; set; }
+
+        [StringLength(255)]
+        public string FullName { get; set; } = null!;
+
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [StringLength(50)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [StringLength(500)]
+        public string ShippingAddress { get; set; } = null!;
+
+        [StringLength(50)]
+        public string Status { get; set; } = null!;
     }
 }
