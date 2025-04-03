d46bf4a53d03643cac8140032e93f9ffb80d6e54
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-03 22:28:10 +0300

    added new migration because i made a typo

---
+++ BeautyLingerieWebApi/BeautyLingerie.Data/Migrations/20250403191236_orderProducttable.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BeautyLingerie.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class orderProducttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Orders_OrderId",
+                table: "OrderProduct");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Products_ProductId",
+                table: "OrderProduct");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderProduct",
+                table: "OrderProduct");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("1be469c3-de77-4975-ba89-7eee4f2621bb"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("3360b8f9-2eef-43bc-8dbd-4658c086f7bd"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("71b5e692-f66e-484c-8310-eefa1aa1de6c"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("8c0f9edd-b8ca-4c3d-9e16-6f8fee3256be"));
+
+            migrationBuilder.RenameTable(
+                name: "OrderProduct",
+                newName: "OrderProducts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_ProductId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_OrderId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_OrderId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderProducts",
+                table: "OrderProducts",
+                column: "OrderProductId");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "ColorId", "CreatedOn", "Description", "ImageUrl", "Name", "Price", "Quantity", "SizeId" },
+                values: new object[,]
+                {
+                    { new Guid("5fdb379c-585c-45c1-81f5-4827d914fc2d"), new Guid("f6e5d4c3-b2a1-4d5e-98c3-769b9b48adf7"), new Guid("37bc840f-355e-4bb2-8b79-ccca593ee1c6"), new DateTime(2025, 4, 3, 19, 12, 35, 771, DateTimeKind.Utc).AddTicks(2718), "Embrace sophistication and comfort with our Elegant White Lace Bra. This exquisite piece features intricate lace detailing and a pristine white color, making it an ideal choice for those who appreciate elegance and quality. Perfect for both everyday wear and special occasions.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/w0hvcjfrp7tcbucmszu1.jpg", "Elegant White Lace Bra", 12.99m, 7, new Guid("c38c1b69-b3b4-4a1b-9e2e-8d5c2d5f0a7e") },
+                    { new Guid("a06d65dc-2ed2-416e-8795-71a81be2f11c"), new Guid("a1b2c3d4-e5f6-4a1b-9e2e-8d5c2d5f0a7e"), new Guid("3f3b5865-bd1e-4a21-9473-7a77d601b0f5"), new DateTime(2025, 4, 3, 19, 12, 35, 771, DateTimeKind.Utc).AddTicks(2696), "Dive into paradise with our Tropical Breeze Bikini, featuring a vibrant blend of blue and pink with a chic palm tree pattern. The set includes a flattering bikini and a matching pink pareo, perfect for effortless beach style and comfort. Ideal for sun-soaked getaways and poolside lounging.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/zaplviqvawybdtxwgoto.jpg", "Tropical Breeze Bikini", 26.00m, 3, new Guid("e2a05d49-c2c4-4ad2-9ae3-f3d1c6a07cb2") },
+                    { new Guid("a1721f5f-f61a-4904-90cd-e420b5be3b9e"), new Guid("9e8b7c6d-5a4b-3c2d-1e0f-a9b8c7d6e5f4"), new Guid("81e5c1be-6c10-44e7-89ed-8c7dd6b3d1a6"), new DateTime(2025, 4, 3, 19, 12, 35, 771, DateTimeKind.Utc).AddTicks(2724), "Elevate your wardrobe with our Chic Black Lace Tank Top. This versatile piece features elegant lace detailing and a sleek black design, perfect for adding a touch of sophistication to any outfit. Ideal for casual outings or dressed-up events.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/rbku1kdduiuluc8jq6qy.jpg", "Chic Black Lace Tank Top", 29.99m, 7, new Guid("d1b82e13-25c1-4d5e-98c3-769b9b48adf7") },
+                    { new Guid("d32067bb-ba8e-4861-bac9-17f89f73cc97"), new Guid("f6e5d4c3-b2a1-4d5e-98c3-769b9b48adf7"), new Guid("48f0be7d-e69d-4933-b81c-0ff4b7106d5d"), new DateTime(2025, 4, 3, 19, 12, 35, 771, DateTimeKind.Utc).AddTicks(2712), "Experience elegance and comfort with our Beautiful Pink Lace Underwear. This stunning piece features delicate lace detailing and a lovely pink hue, making it a perfect choice for those who appreciate beauty and style. Ideal for everyday wear or special occasions.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/jvvbilveebrqwey5vnn9.jpg", "Beautiful Pink Lace Underwear", 3.50m, 5, new Guid("be1e8a73-6b95-4e2e-9673-d4c5a4e8b0da") }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_OrderId",
+                table: "OrderProducts",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_ProductId",
+                table: "OrderProducts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_OrderId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_ProductId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderProducts",
+                table: "OrderProducts");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("5fdb379c-585c-45c1-81f5-4827d914fc2d"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("a06d65dc-2ed2-416e-8795-71a81be2f11c"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("a1721f5f-f61a-4904-90cd-e420b5be3b9e"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("d32067bb-ba8e-4861-bac9-17f89f73cc97"));
+
+            migrationBuilder.RenameTable(
+                name: "OrderProducts",
+                newName: "OrderProduct");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_ProductId",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_OrderId",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_OrderId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderProduct",
+                table: "OrderProduct",
+                column: "OrderProductId");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "ColorId", "CreatedOn", "Description", "ImageUrl", "Name", "Price", "Quantity", "SizeId" },
+                values: new object[,]
+                {
+                    { new Guid("1be469c3-de77-4975-ba89-7eee4f2621bb"), new Guid("a1b2c3d4-e5f6-4a1b-9e2e-8d5c2d5f0a7e"), new Guid("3f3b5865-bd1e-4a21-9473-7a77d601b0f5"), new DateTime(2025, 4, 3, 18, 51, 3, 111, DateTimeKind.Utc).AddTicks(7665), "Dive into paradise with our Tropical Breeze Bikini, featuring a vibrant blend of blue and pink with a chic palm tree pattern. The set includes a flattering bikini and a matching pink pareo, perfect for effortless beach style and comfort. Ideal for sun-soaked getaways and poolside lounging.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/zaplviqvawybdtxwgoto.jpg", "Tropical Breeze Bikini", 26.00m, 3, new Guid("e2a05d49-c2c4-4ad2-9ae3-f3d1c6a07cb2") },
+                    { new Guid("3360b8f9-2eef-43bc-8dbd-4658c086f7bd"), new Guid("f6e5d4c3-b2a1-4d5e-98c3-769b9b48adf7"), new Guid("37bc840f-355e-4bb2-8b79-ccca593ee1c6"), new DateTime(2025, 4, 3, 18, 51, 3, 111, DateTimeKind.Utc).AddTicks(7684), "Embrace sophistication and comfort with our Elegant White Lace Bra. This exquisite piece features intricate lace detailing and a pristine white color, making it an ideal choice for those who appreciate elegance and quality. Perfect for both everyday wear and special occasions.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/w0hvcjfrp7tcbucmszu1.jpg", "Elegant White Lace Bra", 12.99m, 7, new Guid("c38c1b69-b3b4-4a1b-9e2e-8d5c2d5f0a7e") },
+                    { new Guid("71b5e692-f66e-484c-8310-eefa1aa1de6c"), new Guid("9e8b7c6d-5a4b-3c2d-1e0f-a9b8c7d6e5f4"), new Guid("81e5c1be-6c10-44e7-89ed-8c7dd6b3d1a6"), new DateTime(2025, 4, 3, 18, 51, 3, 111, DateTimeKind.Utc).AddTicks(7690), "Elevate your wardrobe with our Chic Black Lace Tank Top. This versatile piece features elegant lace detailing and a sleek black design, perfect for adding a touch of sophistication to any outfit. Ideal for casual outings or dressed-up events.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/rbku1kdduiuluc8jq6qy.jpg", "Chic Black Lace Tank Top", 29.99m, 7, new Guid("d1b82e13-25c1-4d5e-98c3-769b9b48adf7") },
+                    { new Guid("8c0f9edd-b8ca-4c3d-9e16-6f8fee3256be"), new Guid("f6e5d4c3-b2a1-4d5e-98c3-769b9b48adf7"), new Guid("48f0be7d-e69d-4933-b81c-0ff4b7106d5d"), new DateTime(2025, 4, 3, 18, 51, 3, 111, DateTimeKind.Utc).AddTicks(7678), "Experience elegance and comfort with our Beautiful Pink Lace Underwear. This stunning piece features delicate lace detailing and a lovely pink hue, making it a perfect choice for those who appreciate beauty and style. Ideal for everyday wear or special occasions.", "https://res.cloudinary.com/dqko9lpej/image/upload/v1742805902/jvvbilveebrqwey5vnn9.jpg", "Beautiful Pink Lace Underwear", 3.50m, 5, new Guid("be1e8a73-6b95-4e2e-9673-d4c5a4e8b0da") }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Orders_OrderId",
+                table: "OrderProduct",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Products_ProductId",
+                table: "OrderProduct",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
