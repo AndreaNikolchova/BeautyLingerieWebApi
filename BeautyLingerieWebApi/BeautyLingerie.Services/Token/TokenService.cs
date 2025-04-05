20e4ca719e4b0e4071b10f3fed694632a057d160
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-05 16:24:38 +0300

    made token service

---
+++ BeautyLingerieWebApi/BeautyLingerie.Services/Token/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using BeautyLingerie.Services.Token.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyLingerie.Services.Token
+{
+    public class TokenService:ITokenService
+    {
+        private readonly IConfiguration _config;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Secret"]));
+        }
+
+        public string GenerateJwtToken(string email)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, email),
+                new Claim(ClaimTypes.Name, email),
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+    
+    
+
