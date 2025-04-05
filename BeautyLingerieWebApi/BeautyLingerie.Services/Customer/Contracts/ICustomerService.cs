f679c89227883baf35e8aabc3448c40a22be5e52
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-05 16:25:10 +0300

    removed customer controller and addons for it

--- BeautyLingerieWebApi/BeautyLingerie.Services/Customer/Contracts/ICustomerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BeautyLingerie.ViewModels.Customer;
-
-namespace BeautyLingerie.Services.Customer.Contracts
-{
-    public interface ICustomerService
-    {
-        Task AddCustomerAsync(AddCustomerViewModel model);
-    }
-}
