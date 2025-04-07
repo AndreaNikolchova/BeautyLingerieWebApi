67a858ab62f57cf5afd59eea9d89a1b3c2683d60
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-07 13:57:07 +0300

    review model

---
+++ BeautyLingerieWebApi/BeautyLingerie.Data/Models/Review.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BeautyLingerie.Data.Models
+{
+    public class Review
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [MaxLength(1000)]
+        public string? Comment { get; set; }
+        
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
