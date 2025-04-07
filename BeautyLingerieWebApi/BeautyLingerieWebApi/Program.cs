34daf23d1ddb5fceecd16bfb3310d6edc98b0cb3
AndreaNikolchova <a.nikolchova04@gmail.com>
2025-04-07 13:58:49 +0300

    changed the add cors

--- BeautyLingerieWebApi/BeautyLingerieWebApi/Program.cs
+++ BeautyLingerieWebApi/BeautyLingerieWebApi/Program.cs
@@ -97,7 +97,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost", policy =>
     {
-        policy.WithOrigins("http://localhost:5179")
+        policy.WithOrigins("http://localhost:5175")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
